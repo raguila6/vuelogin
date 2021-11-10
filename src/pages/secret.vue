2b4ebbbf91c9f45a8f2d30e69489a97160dda62c
raguila6 <raguila6@stu.southtexascollege.edu>
2021-11-09 23:09:09 -0600

    updated

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,3 +1,3 @@
 <template>
-    <iframe width="420" height="315" src="https://youtu.be/dQw4w9WgXcQ" ></iframe>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </template>
